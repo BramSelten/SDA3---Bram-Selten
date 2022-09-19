c9ce18906ad3834604eddac4846899c74530d670
BramSelten <bram.selten@gmail.com>
2022-09-19 22:14:12 +0200

    middelpunten vinden werkt

--- detect green circle.py
+++ detect green circle.py
@@ -2,11 +2,24 @@ import cv2
 import imutils
 import numpy as np
 
+def nothing(x):
 
+    pass
 
 cap = cv2.VideoCapture(1)
 cap.set(3,640)
-cap.set(4.480)
+cap.set(4,480)
+
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L-H", "Trackbars", 0, 180, nothing)
+cv2.createTrackbar("L-S", "Trackbars", 66, 255, nothing)
+cv2.createTrackbar("L-V", "Trackbars", 134, 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", 180, 180, nothing)
+cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", 243, 255, nothing)
+
+font = cv2.FONT_HERSHEY_COMPLEX
+
 
 
 while True:
@@ -20,8 +33,10 @@ while True:
     u_s = cv2.getTrackbarPos("U-S", "Trackbars")
     u_v = cv2.getTrackbarPos("U-V", "Trackbars")
 
+
     lower_red = np.array([l_h, l_s, l_v])
     upper_red = np.array([u_h, u_s, u_v])
+
 
     mask = cv2.inRange(hsv, lower_red, upper_red)
 
@@ -34,15 +49,18 @@ while True:
         
         M = cv2.moments(c)
         
-        cx = int(M["m10"/M["m00"]])
-        cy = int(M["m01"/M["m00"]])
+        if M["m00"] != 0:
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
+        else:
+        # set values as what you need in the situation
+            cx, cy = 0, 0
         
         cv2.circle(frame, (cx,cy), 7, (255,255,255), -1)
         cv2.putText(frame, "Centre", (cx-20, cy-20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1)
         
-        
-    cv2.imshow("Frame", frame)
-    cv2.imshow("Mask", mask)
+        cv2.imshow("frame", frame)
+   
     
     print("area is....", area)
     print("centroid is at...",cx,cy )
